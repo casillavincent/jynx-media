a7b697e93d1cb89841abbd8a5531ef26a6d8d2b2
Vincent Casilla <73447863+casillavincent@users.noreply.github.com>
2021-05-05 16:17:47 -0700

    Changed useEffect dependency

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -65,7 +65,7 @@ const Header = () => {
          window.removeEventListener("scroll", toggleDarkHeader);
          window.removeEventListener("scroll", checkIsDesktop);
       };
-   });
+   }, []);
 
    return (
       <header
